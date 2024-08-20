1c68824a9d4c308673232240a7b4c1613646a60e
kundup <kundup61@hotmail.com>
2024-08-20 14:35:49 -0700

    basic pygame setup

--- vanphase.py
+++ vanphase.py
@@ -2,20 +2,20 @@ import pygame
 
 # pygame setup
 pygame.init()
-
+clock = pygame.time.Clock()
 # game setups
 set_display_x = 1200
 set_display_y = 700
-pygame.display.set_mode((set_display_x, set_display_y))
+screen = pygame.display.set_mode((set_display_x, set_display_y))
 
 # game loop
 running = True
 while running:
+    screen.fill ("black")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-
     pygame.display.update()
-
+    clock.tick(60)
 pygame.quit()
