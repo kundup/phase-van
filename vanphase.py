1a47c54a6a6e706b178b132852975a1fa54633af
kundup <kundup61@hotmail.com>
2024-08-20 14:31:37 -0700

    basic pygame setup

--- vanphase.py
+++ vanphase.py
@@ -1,3 +1,21 @@
 import pygame
 
+# pygame setup
 pygame.init()
+
+# game setups
+set_display_x = 1200
+set_display_y = 700
+pygame.display.set_mode((set_display_x, set_display_y))
+
+# game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    pygame.display.update()
+
+pygame.quit()
